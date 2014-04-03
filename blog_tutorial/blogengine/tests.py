3870bdb2d5961437792c5374d4167f37c3c6cc41
Trung Doan <trungtrong456@gmail.com>
2014-04-03 10:19:19 +0700

    Added post model with a passing test

--- blog_tutorial/blogengine/tests.py
+++ blog_tutorial/blogengine/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from blogengine.models import Post
+
+
+class PostTest(TestCase):
+
+    def test_create_post(self):
+        # Creates first post to test
+        post = Post()
+        post.title = 'My first post'
+        post.text = 'This is my first blog post'
+        post.pub_date = timezone.now()
+        post.save()
+
+        # Tests if first post saved
+        all_posts = Post.objects.all()
+        self.assertEqual(len(all_posts), 1)
+        only_post = all_posts[0]
+        # Checks first post saved success
+        self.assertEqual(only_post, post)
+        # Check first post content saved success
+        # self.assertEquals(only_post.title, 'My first post')
+        self.assertEquals(only_post.text, 'This is my first blog post')
+        self.assertEquals(only_post.pub_date.day, post.pub_date.day)
+        self.assertEquals(only_post.pub_date.month, post.pub_date.month)
+        self.assertEquals(only_post.pub_date.year, post.pub_date.year)
+        self.assertEquals(only_post.pub_date.hour, post.pub_date.hour)
+        self.assertEquals(only_post.pub_date.minute, post.pub_date.minute)
+        self.assertEquals(only_post.pub_date.second, post.pub_date.second)
